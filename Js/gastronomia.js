1273d286bc62d59f7dd01cf356832f48549f62e6
mayco-castillo <maycoangelocastillo@gmail.com>
2025-07-09 13:30:37 -0300

    primera entrega

---
+++ Js/gastronomia.js
@@ -0,0 +1,29 @@
+let platos = [];
+
+function ingresarPlatos() {
+  let nombre = prompt("Ingrese el nombre del plato");
+  let precio = parseFloat(prompt("Ingrese el precio del plato"));
+  let cantidad = parseInt(prompt("Cantidad de platos pedidos"));
+  platos.push({ nombre, precio, cantidad });
+}
+
+function calcularPrecios() {
+  let total = 0;
+  for (let item of platos) {
+    total += item.precio * item.cantidad;
+  }
+  return total;
+}
+
+function calcularTotal() {
+  console.log("Resumen de pedido");
+  platos.forEach((p) => {
+    console.log(`${p.nombre} x${p.cantidad} - $${p.precio * p.cantidad}`);
+  });
+  alert("Total a pagar: $" + calcularPrecios());
+}
+do {
+  ingresarPlatos();
+} while (confirm("¿Desea agregar otro plato?"));
+
+calcularTotal();
